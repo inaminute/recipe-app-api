542710371a5ea5713ba4b5c0afcf9ba5abcbfe02
SanjayD <sanjayd@mobiux.in>
2021-02-19 11:57:29 +0530

    Normalizing email on new user creation.

--- app/core/models.py
+++ app/core/models.py
@@ -7,7 +7,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         # self._db - It's used for supporting multiple database.
         user.save(using=self._db)
