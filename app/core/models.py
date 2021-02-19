db84794ba6e67cb95df9bc751af88130965390cf
SanjayD <sanjayd@mobiux.in>
2021-02-19 12:08:26 +0530

    Constraining on invalid email cases.

--- app/core/models.py
+++ app/core/models.py
@@ -7,6 +7,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
         """Creates and saves a new user"""
+        if not email:
+            raise ValueError('User must provide email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         # self._db - It's used for supporting multiple database.
